c69ac78c8207d779925195c14c3dc8208a325e0b
Seongchan-CHOI <chltjdcks329@naver.com>
2025-11-26 12:13:28 +0900

    초대장 theme을 선택하는 로직 수정 / 페이지 로딩에서 컴포넌트를 불러오는 형식으로

--- client/src/components/ConditionalLayout.tsx
+++ client/src/components/ConditionalLayout.tsx
@@ -8,7 +8,7 @@ const ConditionalLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
 
   // 이 경로들에서는 하단 네비게이션 바가 나타나지 않습니다.
-  const hideNavBarOn = ['/invitation', '/theme'];
+  const hideNavBarOn = ['/invitation'];
 
   // 현재 경로가 숨김 목록의 경로로 시작하는지 확인합니다.
   const shouldShowNavBar = !hideNavBarOn.some(path => pathname.startsWith(path));
